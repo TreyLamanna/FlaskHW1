ce7125465271c0d0e28886aea8016e3d2b1ee717
TreyLamanna <111934156+TreyLamanna@users.noreply.github.com>
2024-10-30 18:07:37 -0500

    updated app.py with delete button and function

--- app.py
+++ app.py
@@ -102,6 +102,21 @@ def edit(id):
     
     return render_template('edit.html', post=post)
 
+#create a route to delete a post
+@app.route('/<int:id>/delete/', methods=('POST',))
+def delete(id):
+    post = get_post(id)
+
+    conn = get_db_connection()
+    conn.execute('DELETE from posts WHERE id = ?', (id,))
+    conn.commit()
+    conn.close()
+
+    flash('"{}" was successfully deleted'.format(post['title']))
+
+    return redirect(url_for('index'))
+
+
 
 
 app.run(port=5008)
