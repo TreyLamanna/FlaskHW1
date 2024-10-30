e6c2108d830801fb7e62bc1872e90d15d89561b6
TreyLamanna <111934156+TreyLamanna@users.noreply.github.com>
2024-10-30 17:29:09 -0500

    updated app.py with post and index.html

--- app.py
+++ app.py
@@ -6,8 +6,6 @@ app = Flask(__name__)
 app.config["DEBUG"] = True
 app.config['SECRET_KEY'] = 'your secret key'
 
-
-
 # Function to open a connection to the database.db file
 def get_db_connection():
     # create connection to the database
@@ -19,6 +17,17 @@ def get_db_connection():
 
     #return the connection object
     return conn
+
+#Function to retrieve a post from the database
+def get_post(post_id):
+    conn = get_db_connection()
+    post = conn.execute('SELECT * FROM posts WHERE id = ?', (post_id,)).fetchone()
+    conn.close()
+
+    if post is None:
+        abort(404)
+
+    return post
 
 
 # use the app.route() decorator to create a Flask view function called index()
@@ -63,6 +72,36 @@ def create():
 
 
     return render_template('create.html')
+
+#create a route to edit a post. Load page with get or post method
+#pass the post id as a URL parameter
+@app.route('/<int:id>/edit/', methods=('GET', 'POST'))
+def edit(id):
+    #get the posts from the database
+    post = get_post(id)
+
+    #determine if the page was request with GET or POST
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+
+        if not title:
+            flash('Title is required')
+        elif not content:
+            flash('Content is required')
+        else:
+            conn = get_db_connection()
+            conn.execute('UPDATE posts SET title = ?, content = ? WHERE id = ?', (title, content, id))
+            conn.commit()
+            conn.close()
+
+            return redirect(url_for('index'))
+
+    #If GET then display page
+    #If POST, process from the data. Get the data validate it.
+    
+    return render_template('edit.html', post=post)
+
 
 
 app.run(port=5008)
